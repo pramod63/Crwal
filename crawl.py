55d305ab31a07ab45138c60eff205c79086802ee
Pramod Naik <38611348+pramod63@users.noreply.github.com>
2019-04-08 15:53:03 +0530

    added third and fourth stage

--- crawl.py
+++ crawl.py
@@ -1,2 +1,4 @@
 print('This is start up for crawl')
 print('Updated from first stage to second stage')
+print('Updated from second stage to second stage')
+print('Updated from third stage to second stage')
